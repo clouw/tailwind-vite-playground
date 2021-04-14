42c71849cc93f3d3688c03e394b95a87866f79a0
Ramón Soto <ddsstr@gmail.com>
2021-04-13 21:28:08 -0400

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,6 @@
-// const purgecss = require('@fullhuman/postcss-purgecss')({
-//     content: [
-//       './src/**/*.html'
-//     ],
-
-//     defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-//   });
-
-//   module.exports = {
-//     plugins: [
-//       require('tailwindcss')('./tailwind.config.js'),
-//       require('autoprefixer'),
-//       ...process.env.NODE_ENV === 'production' ? [purgecss] : []
-//     ]
-//   }
-
 module.exports = {
-  purge: ["./src/index.html"],
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-};
+}
